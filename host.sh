9cda1d54abdd4d121573ec1a8bdcb3a87f1fbca4
Dorian Jaminais-Grellier <azure@jaminais.fr>
2020-07-16 19:47:49 +0000

    rename rc file

--- host.sh
+++ host.sh
@@ -3,3 +3,5 @@ set -eux
 sudo apt-get install less
 
 pip3.8 install --user pipenv pre-commit ipython
+
+grep "nanassrc" ~/.bashrc || echo "source $(dirname "${BASH_SOURCE[0]}")/nanassrc" >> ~/.bashrc
